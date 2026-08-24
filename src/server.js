000523cc764eac30748eb6ff3414a37f125fa7f8
jpsalez <joaopedrosalesdev@gmail.com>
2026-08-24 18:22:05 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(process.env.PORT, () => {
+    console.log('server is on in port 3000');
+});
+
